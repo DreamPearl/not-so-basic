8d1f5740ddc0f440596208f8daddba85e3391774
Rakhi Kumari <rakhi.cs61@gmail.com>
2022-09-06 17:55:58 +0530

    Update heap.cpp

--- cpp/heap.cpp
+++ cpp/heap.cpp
@@ -7,6 +7,7 @@ int main() {
     std::cout << "The elements in the vector are: " << std::endl;
     for(int i=0;i<5;i++) {
         std::cout << num[i];
+        std::cout << "  ";
     }
     std::cout << std::endl;
 
@@ -16,26 +17,39 @@ int main() {
     std::cout << "Displaying the heap elements: " << std::endl;
     for(int i=0;i<5;i++) {
         std::cout << num[i];
+        std::cout << "  ";
     }
     std::cout << std::endl;
 
-    num.push_back(8);
+    num.push_back(10);
     std::push_heap(num.begin(), num.end());
 
-    std::cout << "The elements in the heap after push_back: " << std::endl;
+    std::cout << "The elements in the vector after push_back/push_heap: " << std::endl;
     for(int i=0;i<num.size();i++) {
         std::cout << num[i];
+        std::cout << "  ";
     }
     std::cout << std::endl;
 
     std::pop_heap(num.begin(), num.end());
     num.pop_back();
 
-    std::cout << "The elements in the heap after pop_back: " << std::endl;
+    std::cout << "The elements in the vecor after pop_heap/pop_back: " << std::endl;
     for(int i=0;i<num.size();i++) {
         std::cout << num[i];
+        std::cout << "  ";
     }
     std::cout << std::endl;
-    
+
+    // Only doing pop_heap will make the largest, i.e front() element, element to go in the last of the vector.
+    // While all other elements will continue to preserve the properties of the heap
+    std::pop_heap(num.begin(), num.end());
+    std::cout << "The elements in the vecor after pop_heap only: " << std::endl;
+    for(int i=0;i<num.size();i++) {
+        std::cout << num[i];
+        std::cout << "  ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
