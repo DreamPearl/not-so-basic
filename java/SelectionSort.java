aee6ff767d4e32ab9c2066cd353e38a598ef945a
Rakhi Kumari <rakhi.cs61@gmail.com>
2022-12-23 14:31:53 +0530

    Add selection sort in java

---
+++ java/SelectionSort.java
@@ -0,0 +1,24 @@
+public class SelectionSort {
+    public static void selectionSort(int arr[]) {
+        for(int i=0;i<arr.length-1;i++) {
+            int index = i;
+            for(int j=i+1;j<arr.length;j++) {
+                if(arr[j]<arr[index]) {
+                    index = j;
+                }
+            }
+            int smallestNum = arr[index];
+            arr[index] = arr[i];
+            arr[i] = smallestNum;
+        }
+    }
+
+    public static void main(String a[]) {
+        int arr[] = {27, 6, 7, 10, 19, 22, 40, 25};
+        selectionSort(arr);
+        System.out.println("Sorted array using Selection sort: ");
+        for(int i = 0;i < arr.length;i++) {
+            System.out.println(arr[i]+" ");
+        }
+    }
+}
